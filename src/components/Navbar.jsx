b3fdf3390b878ef745586e02fc53b4371295437a
Ricardo Farid Perez Coyote <ti44793@uvp.edu.mx>
2025-05-15 00:16:14 -0600

    test

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,13 @@
+// src/components/Navbar.tsx
 import React from 'react';
 import './Navbar.css';
+import { SITE_BASE } from '../config';
 
 const Navbar = () => {
   return (
     <nav className="navbar navbar-expand-lg my-navbar">
       <div className="container">
-        <a className="navbar-brand" href="/"><img src="/logo2.svg" alt="logo" /></a>
+        <a className="navbar-brand" href={SITE_BASE}><img src={`${SITE_BASE}`+'/logo2.svg'} alt="logo" /></a>
         <button
           className="navbar-toggler"
           type="button"
@@ -20,16 +22,16 @@ const Navbar = () => {
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav ms-auto">
             <li className="nav-item">
-              <a className="nav-link" href="/">Inicio</a>
+              <a className="nav-link" href={SITE_BASE}>Inicio</a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="/juegos">Juegos</a>
+              <a className="nav-link" href={`${SITE_BASE}`+'/juegos'}>Juegos</a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="/nosotros">Nosotros</a>
+              <a className="nav-link" href={`${SITE_BASE}`+'/nosotros'}>Nosotros</a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="/ayuda">Ayuda</a>
+              <a className="nav-link" href={`${SITE_BASE}`+'/ayuda'}>Ayuda</a>
             </li>
           </ul>
         </div>
---
+++ src/config.jsx
@@ -0,0 +1,1 @@
+export const SITE_BASE = import.meta.env.BASE_URL;
