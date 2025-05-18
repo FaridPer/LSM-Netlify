1855cda0ba54ae13adbe84f3b3c2ae361ddd4285
Ricardo Farid Perez Coyote <ti44793@uvp.edu.mx>
2025-05-18 15:03:52 -0600

    test

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 
+import netlify from '@astrojs/netlify';
+
 export default defineConfig({
   integrations: [react()],
   site: 'https://faridper.github.io',
   base: '/LSM/',
+  adapter: netlify(),
 });
